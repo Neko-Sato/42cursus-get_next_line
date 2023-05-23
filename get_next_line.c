af7d120243393eb5a3d2204a88232b37d1b50575
Neko-Sato <hisatosi35350@gmail.com>
2023-05-23 09:34:08 +0900

    とりあえず雰囲気をつかむために

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,60 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 19:34:00 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/05/22 19:34:09 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/05/23 00:36:11 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd)
+{
+	char		buf[BUFFER_SIZE];
+	static char	*temp;
+	char		*_temp;
+	char		*enter;
+	char		*result;
+	ssize_t		size;
+
+	//	tempを空文字に初期化
+	if (!temp)
+		temp = ft_strdup("");
+	//	改行をなしに初期化
+	enter = NULL;
+	while (!enter)
+	{
+		//	bufを空の文字列に初期化
+		buf[0] = '\0';
+		//	tempから改行を検索
+		enter = ft_strchr(temp, '\n');
+		//	tempに改行がなければ
+		if (!enter)
+		{
+			size = read(fd, buf, BUFFER_SIZE);
+			//	bufに読み取りそこから改行を探す
+			if(size == 0 && *temp)
+				enter = buf + size;
+			else if (size <= 0)
+				return (NULL);
+			else
+				enter = ft_strchr(buf, '\n');
+		}
+		//	改行があればそれを終端文字に変える
+		if (enter)
+			*enter = '\0';
+		//	tempとbufをつなげた文字を作る
+		result = ft_strjoin(temp, buf);
+		//	改行があればtempを改行から一つ先の文字列にする
+		_temp = temp;
+		if (enter)
+			temp = ft_strdup(enter + 1);
+		else
+			temp = result;
+		free(_temp);
+		//	改行があれば抜ける
+	}
+	return (result);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,18 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 19:34:02 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/05/22 19:34:08 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/05/22 21:03:30 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+char	*get_next_line(int fd);
+
+int		ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
