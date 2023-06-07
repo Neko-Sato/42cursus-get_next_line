e0f0ed4b83796caddaf1cb43455e5bbb7d606493
hshimizu <hisatosi35350@gmail.com>
2023-06-07 22:55:53 +0900

    バッファーサイズ未定義時にも実行可能に

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 19:34:02 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/06/05 16:00:49 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/06/07 22:55:05 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
 
