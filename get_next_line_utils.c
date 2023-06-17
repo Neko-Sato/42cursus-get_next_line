b40269b0245d825c1a11cfad2645ab43da73916c
hshimizu <hisatosi35350@gmail.com>
2023-06-17 21:07:50 +0900

    マロック処理忘れ

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 19:34:06 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/06/05 15:24:31 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/06/17 21:07:12 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,10 +63,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		s2 = "";
 	result = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	_result = result;
-	while (*s1)
-		*result++ = *s1++;
-	while (*s2)
-		*result++ = *s2++;
-	*result = '\0';
+	if (result)
+	{
+		while (*s1)
+			*result++ = *s1++;
+		while (*s2)
+			*result++ = *s2++;
+		*result = '\0';
+	}
 	return (_result);
 }
